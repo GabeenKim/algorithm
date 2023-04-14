78b0f8dec0b9bf0f4b5ce9dabd004da74885375a
GabeenKim <rlarkqls5551250@gmail.com>
2023-04-14 12:43:43 +0900

    예제2 정렬된 배열에서 특정 수 구하기

---
+++ 이코테_예제/binary_search/ex2_count_in_sorting_arr.py
@@ -0,0 +1,15 @@
+from bisect import bisect_left, bisect_right
+
+def count_by_range(array,x):
+    right_index = bisect_right(array,x)
+    left_index = bisect_left(array,x)
+    return right_index - left_index
+
+n , x = map(int,input().split())
+array = list(map(int,input().split()))
+
+count = count_by_range(array, x)
+if count == 0:
+    print(-1)
+else :
+    print(count)
