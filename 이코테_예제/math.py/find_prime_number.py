79a843642797d07fb79ad5b2bd631869931cb723
GabeenKim <rlarkqls5551250@gmail.com>
2023-04-20 18:30:16 +0900

    소수 판별 기본 알고리즘

---
+++ 이코테_예제/math.py/find_prime_number.py
@@ -0,0 +1,10 @@
+def is_prime_number(x) :
+    #2부터 x-1까지의 모든 수 확인하기 
+    for i in range(2,x) :
+        #x가 해당 수로 나누어 떨어진다면 소수 아님
+        if x % i == 0 :
+            return False
+        else : return True
+
+print(is_prime_number(4))
+print(is_prime_number(7))
