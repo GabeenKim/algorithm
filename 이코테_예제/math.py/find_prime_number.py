0c5aab23c87aa6e5e15cb1c3cdbe717829f6b097
GabeenKim <rlarkqls5551250@gmail.com>
2023-04-20 18:32:34 +0900

    약수 성질 이용 소수 판별

--- 이코테_예제/math.py/find_prime_number.py
+++ 이코테_예제/math.py/find_prime_number.py
@@ -1,6 +1,9 @@
+#약수의 성질 이용해서 소수 판별
+import math 
+
 def is_prime_number(x) :
     #2부터 x-1까지의 모든 수 확인하기 
-    for i in range(2,x) :
+    for i in range(2,int(math.sqrt(x))+1) :
         #x가 해당 수로 나누어 떨어진다면 소수 아님
         if x % i == 0 :
             return False
