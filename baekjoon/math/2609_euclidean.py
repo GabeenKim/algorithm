a371ca5a6cb4f3cbd5bb550fc6f8f33aa0f2938b
GabeenKim <rlarkqls5551250@gmail.com>
2023-04-24 20:12:17 +0900

    gcd,lcd 유클리드호제법

---
+++ baekjoon/math/2609_euclidean.py
@@ -0,0 +1,13 @@
+#유클리드 호제법 이용
+a, b = map(int,input().split())
+
+def gcd(a,b):
+  while b > 0:
+    a,b = b, a%b
+  return a
+
+def lcd(a,b):
+  return a*b//gcd(a,b)
+
+print(gcd(a,b))
+print(lcd(a,b))
