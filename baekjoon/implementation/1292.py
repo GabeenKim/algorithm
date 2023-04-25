cc79d1512d9a0bc3c2cc0e987f9b89d4c2d981f3
GabeenKim <rlarkqls5551250@gmail.com>
2023-04-25 21:09:43 +0900

    쉽게푸는문제(구간합)_슬라이싱이용

--- baekjoon/implementation/1292.py
+++ baekjoon/implementation/1292.py
@@ -1,14 +1,8 @@
 a, b = map(int, input().split())
 
-arr = [1]
-sum = 0
-for i in range(2, 46):
+arr=[]
+for i in range(46):
     for j in range(i):
         arr.append(i)
 
-pre_fix = [0]
-for i in arr:
-    sum += i
-    pre_fix.append(sum)
-
-print(pre_fix[b] - pre_fix[a - 1])
+print(sum(arr[a-1:b]))
