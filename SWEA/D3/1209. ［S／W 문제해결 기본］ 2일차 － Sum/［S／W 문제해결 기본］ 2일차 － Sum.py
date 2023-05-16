61582fd60f92d4b81a5bb14cf5979cfeb4e4863a
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-05-16 21:28:36 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 188 ms, Memory: 60,496 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,28 +1,22 @@
 for _ in range(1,11):
     tc = int(input())
-    arr = [[0]*100 for _ in range(100)]
+    arr = [list(map(int, input().split())) for _ in range(100)]
 
-    for i in range(100):
-        arr[i] = list(map(int, input().split()))
-
-    sum = 0
     max_sum=[]
-    diagonal_left , diagonal_right = 0,0
-    for i in range(100):
-        for j in range(100):
-            sum += arr[i][j]
-            if j == 99 :
-                max_sum.append(sum)
-                sum = 0
-            if i == j :
-                diagonal_left += arr[i][j]
-    for i in range(100):
-        for j in range(100):
-            sum += arr[j][i]
-            if j == 99 :
-                max_sum.append(sum)
-                sum = 0
-            if i == j:
-                diagonal_right += arr[j][i]
 
-    print(f'#{tc} {max(max(max_sum),diagonal_right,diagonal_left)}')
+    # row, col
+    for i in range(100):
+        row_sum, col_sum = 0, 0
+        for j in range(100):
+            row_sum += arr[i][j]
+            col_sum += arr[j][i]
+            if j == 99 :
+                max_sum.append(row_sum)
+                max_sum.append(col_sum)
+    # diagonal
+    diagonal_left, diagonal_right = 0, 0
+    for i in range(100):
+        diagonal_left += arr[i][i]
+        diagonal_right += arr[i][99-i]
+
+    print(f'#{tc} {max(max_sum)}')
