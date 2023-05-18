3750dac0fee04a8fbdf151d2adb7ef8a28d1c51c
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-05-18 20:27:24 +0900

    [D3] Title: 농작물 수확하기, Time: 240 ms, Memory: 64,616 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -2,19 +2,18 @@ t = int(input())
 for tc in range(1,t+1):
     n = int(input())
     arr = [list(map(int, input())) for _ in range(n)]
-
     ans = 0
-    for i in range(n//2+1):
-        start = (n// 2) - i
-        end = (n// 2) + i + 1
-        num = arr[i][start : end]
-        for j in num :
-            ans += j
 
-    for i in range(n//2+1,n):
-        start = i - (n // 2)
-        end = n - start
-        num = arr[i][start: end]
-        for j in num:
-            ans += j
+    start, end = n // 2, n // 2
+
+    for i in range(n):
+        for j in range(start, end+1):
+            ans += arr[i][j]
+        if i < n//2 :
+            start -= 1
+            end += 1
+        else :
+            start += 1
+            end -= 1
+            
     print(f'#{tc} {ans}')
