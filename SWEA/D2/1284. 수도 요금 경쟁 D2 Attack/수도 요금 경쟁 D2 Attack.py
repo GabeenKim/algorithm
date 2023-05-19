a0affee9c4938b1c362ec64dd13bbe5282bd3e07
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-05-19 14:04:05 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 155 ms, Memory: 58,268 KB -BaekjoonHub

--- SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
@@ -5,11 +5,8 @@ for tc in range(1, t+1):
     ans = 0
     if w > r :
         b_price = q + (w-r)*s
-        if a_price < b_price : ans = a_price
-        else : ans = b_price
-    else :
-        if a_price < b_price:
-            ans = a_price
-        else:
-            ans = b_price
+    if a_price < b_price:
+        ans = a_price
+    else:
+        ans = b_price
     print(f'#{tc} {ans}')
