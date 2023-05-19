bc602f1dcc88306d578cf75a2ec1c6827270c13b
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-05-20 04:28:07 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 150 ms, Memory: 58,772 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -1,20 +1,18 @@
 for tc in range(1,11):
     length = int(input())
     arr = [list(input()) for _ in range(8)]
+
     cnt = 0
+    std = length // 2 if length%2== 0 else (length//2)+1
 
     for i in arr:
         for j in range(9-length):
-            s = i[j:j + length]
-            comp = s[::-1]
-            if comp == s :
+            if i[j+length-1 : j+length-1-std : -1] == i[j:j + std]:
                 cnt += 1
 
     for i in zip(*arr):
         for j in range(9-length):
-            s2 = i[j:j + length]
-            comp2 = s2[::-1]
-            if comp2 == s2 :
+            if i[j+length-1 : j+length-1-std : -1] == i[j:j + std]:
                 cnt += 1
 
     print(f'#{tc} {cnt}')
