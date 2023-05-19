e96d8f3c4310abcd7ecba7017c44443594b7eef8
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-05-20 04:17:15 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 172 ms, Memory: 58,520 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -3,22 +3,18 @@ for tc in range(1,11):
     arr = [list(input()) for _ in range(8)]
     cnt = 0
 
-    col = [[] for _ in range(8)]
-    for i in range(8):
-        for j in range(8):
-            col[i].append(arr[j][i])
-
-    for i in range(8):
+    for i in arr:
         for j in range(9-length):
-            s = arr[i][j:j + length]
-            s2 = col[i][j:j + length]
-
+            s = i[j:j + length]
             comp = s[::-1]
-            comp2 = s2[::-1]
-
             if comp == s :
                 cnt += 1
-            if comp2 == s2:
-                cnt +=1
+
+    for i in zip(*arr):
+        for j in range(9-length):
+            s2 = i[j:j + length]
+            comp2 = s2[::-1]
+            if comp2 == s2 :
+                cnt += 1
 
     print(f'#{tc} {cnt}')
