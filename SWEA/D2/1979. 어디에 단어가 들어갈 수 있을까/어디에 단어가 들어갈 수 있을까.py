35fbc26c03bc524b84d3cf9b5ea09d3c5b9ed8dc
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-05-20 13:05:37 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 161 ms, Memory: 60,972 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -2,29 +2,15 @@ t = int(input())
 
 for tc in range(1, t+1):
     n, k = map(int, input().split())
-    arr = [list(map(int,input().split())) for _ in range(n)]
+    arr = [input() for _ in range(n)]
 
     row, col = 0,0
     cnt = 0
 
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j] == 1:
-                row +=1
-            if arr[i][j] == 0:
-                if row == k :
-                    cnt +=1
-                row = 0
-            if arr[j][i] == 1:
-                col +=1
-            if arr[j][i] == 0 :
-                if col == k:
-                    cnt +=1
-                col = 0
-        # k번째 1이 행or열의 마지막 열에 있을 경우
-        if row == k :
-            cnt +=1
-        if col == k :
-            cnt += 1
-        row,col = 0,0
+    for i in arr :
+        cnt += i.replace(' ', '').split('0').count('1'*k)
+
+    for i in list(zip(*arr)):
+        cnt +=''.join(i).split('0').count('1'*k)
+
     print(f'#{tc} {cnt}')
