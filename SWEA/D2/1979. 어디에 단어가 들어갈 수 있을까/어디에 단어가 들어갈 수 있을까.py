d5c1a398367ac1f92d4803aaeda01915a6270650
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-05-20 12:54:30 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 170 ms, Memory: 61,464 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -4,32 +4,27 @@ for tc in range(1, t+1):
     n, k = map(int, input().split())
     arr = [list(map(int,input().split())) for _ in range(n)]
 
+    row, col = 0,0
     cnt = 0
-    s = []
 
-    for row in arr:
-        for j in range(n-k+1):
-            if row[j:j+k].count(1) == k :
-                if j-1 < 0 :
-                    if row[j+k] == 0:
-                        cnt +=1
-                elif j+k >=n :
-                    if row[j - 1] == 0:
-                        cnt += 1
-                else:
-                    if row[j-1] == 0 and row[j+k] == 0 :
-                       cnt+=1
-
-    for row in zip(*arr):
-        for j in range(n - k + 1):
-            if row[j:j + k].count(1) == k:
-                if j - 1 < 0:
-                    if row[j + k] == 0:
-                        cnt += 1
-                elif j + k >= n:
-                    if row[j - 1] == 0:
-                        cnt += 1
-                else:
-                    if row[j - 1] == 0 and row[j + k] == 0:
-                        cnt += 1
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] == 1:
+                row +=1
+            if arr[i][j] == 0:
+                if row == k :
+                    cnt +=1
+                row = 0
+            if arr[j][i] == 1:
+                col +=1
+            if arr[j][i] == 0 :
+                if col == k:
+                    cnt +=1
+                col = 0
+        # k번째 1이 행or열의 마지막 열에 있을 경우
+        if row == k :
+            cnt +=1
+        if col == k :
+            cnt += 1
+        row,col = 0,0
     print(f'#{tc} {cnt}')
