9a1f79bcb4fce134cf8be1af94c64815f5602dc2
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-05-24 20:31:48 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 145 ms, Memory: 58,504 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
@@ -2,12 +2,13 @@ t = int(input())
 
 for tc in range(1,t+1):
     memory = input()
-    cur = ['0'] * len(memory)
-    ans = 0
-    for i in range(len(memory)):
-        if memory[i] != cur[i]:
-            ans += 1
-            for k in range(i, len(memory)):
-                cur[k] = memory[i]
-            
+    # 0번째 bit가 1이면 1111처럼 복구해야하므로 cnt +1
+    if memory[0] == '0':
+        ans = 0
+    else : ans = 1
+
+   # 앞과 뒤의 비트가 다를 때마다 복구작업을 해야하므로 두 자리만 비교해도 됨.
+    for i in range(len(memory) -1):
+        if memory[i] != memory[i+1]:
+            ans+=1
     print(f'#{tc} {ans}')
