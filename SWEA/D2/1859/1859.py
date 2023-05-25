d83a0ba7083d410422ece455bf9988e9920eff09
GabeenKim <rlarkqls5551250@gmail.com>
2023-05-25 16:42:18 +0900

    .

--- SWEA/D2/1859/1859.py
+++ SWEA/D2/1859/1859.py
@@ -1,17 +1,13 @@
-# max() 시간복잡도 O(n)임을 유의! 
 t = int(input())
-for t in range(1, t+ 1):
+for test_case in range(1,t+1):
     n = int(input())
-    arr = list(map(int, input().split()))
-
+    price = list(map(int, input().split()))
     benefit = 0
-    buy = []
-    for i in range(n):
-        # 본인을 기준으로 뒤에 큰 값이 있으면 스택에 삽입
-        if any(arr[i] < arr[j] for j in range(i + 1, n)):
-            buy.append(arr[i])
+    max_num = price[-1]
+    for i in range(len(price) - 2, -1, -1):
+        if price[i] > max_num:
+            max_num = price[i]
         else:
-            while buy != []:
-                benefit += arr[i] - buy.pop()
+            benefit += max_num - price[i]
 
-    print(f'#{t} {benefit}')
+    print(f'#{test_case} {benefit}')
