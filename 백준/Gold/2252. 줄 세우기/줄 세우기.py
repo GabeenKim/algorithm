1970ad3423860e34cfb16955f964c731f4afd837
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-06-01 15:49:54 +0900

    [Gold III] Title: 줄 세우기, Time: 200 ms, Memory: 38724 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,41 +1,39 @@
 from collections import deque
-import sys 
+import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-# 진입차수 0으로 초기화
-indegree = [0] * (n+1)
-
-graph = [[] for _ in range(n+1)]
-
-for i in range(m):
-  # a <- b
-  a, b = map(int,input().split())
-  graph[a].append(b)
-  # a가 b 앞에 서 있으므로 a->b
-  indegree[b] += 1
-
 def topology():
-  result = [] 
+  #정렬결과. 큐에 삽입되는 순서가 정렬 순서
+  result = []
   q = deque()
 
-  # 진입 차수가 0인 노드를 큐에 삽입
-  for i in range(1, n+1):
-    if indegree[i] == 0 :
+  # 진입차수가 0인 노드 큐에 삽입
+  for i in range(1,n + 1):
+    if indegree[i] == 0:
       q.append(i)
-
-  while q :
-    # 큐에서 원소 꺼내기 
+  # 큐에서 꺼낸 것을 결과 리스트에 삽입. 연결된 노드들의 진입차수 제거-> 진입차수 0이 된 것 새로이 삽입 반복
+  while q:
     now = q.popleft()
     result.append(now)
 
-    # 해당 원소에서 나가는 간선 제거. 즉 연결 노드들의 진입차수 -1
-    for i in graph[now]:
-      indegree[i] -= 1 
-      # 새롭게 진입 차수가 0이 된 노드 큐에 삽입
-      if indegree[i] == 0 :
+    for i in edge[now]:
+      indegree[i] -= 1
+      if indegree[i] == 0:
         q.append(i)
 
   print(*result)
+
+
+n, m = map(int, input().split())
+
+#진입차수 변수
+indegree = [0] * (n + 1)
+#방향 그래프의 간선 정보 입력 받기
+edge = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+  a, b = map(int, input().split())
+  edge[a].append(b)
+  indegree[b] += 1
 
 topology()
