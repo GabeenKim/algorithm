448c0f3be8970d347a797d3fc82d3c000c14368b
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-06-05 13:36:26 +0900

    [Gold V] Title: 동전 2, Time: 392 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,18 +1,17 @@
-# 복습 필 
 import sys
 input = sys.stdin.readline
 INF = int(1e9)
 
 n, k = map(int,input().split())
-coin = [int(input()) for _ in range(n)]
+coins = [int(input()) for _ in range(n)]
 
+#k원을 만들 수 있는 경우의 수 dp[k]
 dp = [INF]*(k+1)
-dp[0] = 0
+dp[0]=0
 
-for c in coin : 
-  for i in range(c, k+1):
-    if dp[i-c] != INF:
-      dp[i] = min(dp[i], dp[i-c]+1)
+for coin in  coins:
+  for i in range(coin,k+1):
+    dp[i] = min(dp[i], dp[i-coin]+1)
 
-if dp[k] == INF : print(-1)
-else : print(dp[k])
+if dp[k]!= INF : print(dp[k])
+else : print(-1)
