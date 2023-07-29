25b8b93ccb1f92ec25de0b66107d389613f9cf56
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-07-29 12:05:30 +0900

    [Silver III] Title: 프린터 큐, Time: 160 ms, Memory: 14856 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,13 +1,14 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,18 +18,19 @@ public class Main {
 		
 		for(int tc=0; tc<TC; tc++) {
 			Queue<Integer> queue = new LinkedList<Integer>();
-			LinkedList<Integer> arrIdx = new LinkedList<>();
+			ArrayList<Integer> arrIdx = new ArrayList<>();
 			
 			st = new StringTokenizer(br.readLine()," ");
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
 			
+			//입력 순서대로 큐에 삽입하고 인덱스를 리스트에 저장 
 			st = new StringTokenizer(br.readLine(), " ");
 			for(int i=0;i<N;i++) {
 				queue.offer(Integer.parseInt(st.nextToken()));
 				arrIdx.add(i);
 			}
-			//1개일 때 처리
+			//문서가 1개일 때는 중요도를 비교할 필요가 X 따라서 다음 테스트 케이스로 ...
 			if (N==1) {
 				queue.poll();
 				System.out.println(1);
@@ -44,12 +46,12 @@ public class Main {
 				//maxidx만큼 삭제 삽입 반복 
 				for(int i=0;i<maxIdx;i++) {
 					int num = queue.poll();
-					int numIdx = arrIdx.removeFirst();
+					int numIdx = arrIdx.remove(0);
 					queue.offer(num);
 					arrIdx.add(numIdx);
 				}
 				queue.poll();
-				int temp = arrIdx.removeFirst();
+				int temp = arrIdx.remove(0);
 				if(temp==M) {
 					cnt++;
 					break;
@@ -62,4 +64,5 @@ public class Main {
 		
 		
 	}
+
 }
