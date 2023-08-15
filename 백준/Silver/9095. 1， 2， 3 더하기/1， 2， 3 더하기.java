62a3db6f4b5553a6b119209bf2fd5ca29db1c54e
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-08-15 15:29:29 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 132 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -22,19 +22,14 @@ public class Main {
 		}//tc
 	}
 
-	/*
-	 	1,2,3의 합으로 N을 이루는 개수는 다음의 규칙을 따른다. 단, n>=4인 경우에만 
-	 	N = N-1번째의 경우의 수 + N-2번째의 경우의 수 + N-3번째의 경우의 수 
-	 */
 	public static int dfs(int n) {
-		//n<3인 경우 초기값으로 다음과 같이 주어짐. 
-		if(n== 1 || n== 2) return dp[n]=n;
-		else if(n==3) return dp[n]=4;
+		if(n<3) return n;
+		if(n==3) return 4;
 		
-		if(dp[n]!=0) return dp[n];
-		else {
-			//n>=4이고 dp 테이블에 값이 없는 경우 재귀탐색 실행
-			return dp[n]=dfs(n-1)+dfs(n-2)+dfs(n-3);
+		dp[1]=1; dp[2]=2; dp[3]=4;
+		for(int i=4;i<=n;i++) {
+			dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
 		}
+		return dp[n];
 	}
 }
