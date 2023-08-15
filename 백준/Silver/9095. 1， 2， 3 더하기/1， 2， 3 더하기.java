e74d2925ff3d0da5c0376b7e214397973b7ca268
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-08-15 15:23:14 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 136 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -4,8 +4,8 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main{
-	static int N;
+public class Main {
+	static int[] dp;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,16 +14,27 @@ public class Main{
 		
 		for(int tc =0; tc<t;tc++) {
 			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			
+			dp = new int[N+1];
 			
 			System.out.println(dfs(N));
 		}//tc
 	}
 
+	/*
+	 	1,2,3의 합으로 N을 이루는 개수는 다음의 규칙을 따른다. 단, n>=4인 경우에만 
+	 	N = N-1번째의 경우의 수 + N-2번째의 경우의 수 + N-3번째의 경우의 수 
+	 */
 	public static int dfs(int n) {
-		if(n== 1 || n== 2) return n;
-		else if(n==3) return 4;
+		//n<3인 경우 초기값으로 다음과 같이 주어짐. 
+		if(n== 1 || n== 2) return dp[n]=n;
+		else if(n==3) return dp[n]=4;
 		
-		return dfs(n-1)+dfs(n-2)+dfs(n-3);
+		if(dp[n]!=0) return dp[n];
+		else {
+			//n>=4이고 dp 테이블에 값이 없는 경우 재귀탐색 실행
+			return dp[n]=dfs(n-1)+dfs(n-2)+dfs(n-3);
+		}
 	}
 }
