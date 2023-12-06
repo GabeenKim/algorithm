c4257e87e29d05e15c78ed3c846631812617e4c6
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2023-12-06 15:53:10 +0900

    [level 2] Title: 전화번호 목록, Time: 230.66 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,11 +1,13 @@
-import java.util.Arrays;
+import java.util.HashMap;
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
-        
-        for(int i=1;i<phone_book.length ;i++){
-            if(phone_book[i].startsWith(phone_book[i-1])){
-                return false;
+        HashMap<String,Integer> hm = new HashMap<>();
+        for(int i=0; i<phone_book.length;i++)
+            hm.put(phone_book[i],i);
+        for(String value : phone_book){
+            for(int i=1; i<value.length() ;i++){
+                if(hm.containsKey(value.substring(0,i)))
+                    return false;
             }
         }
         
