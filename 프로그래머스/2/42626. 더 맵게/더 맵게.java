3f73feb123f39022a82c975d2f432291cf490eb4
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2025-08-10 11:32:26 +0900

    [level 2] Title: 더 맵게, Time: 1601.04 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,14 +2,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        Queue<Integer> queue = new PriorityQueue<>();
         
         for(int i=0; i<scoville.length;i++){
             queue.add(scoville[i]);
         }
         
+        //모든 음식의 스코빌 지수가 K이상이 되면 끝
         while(queue.peek()<K){
-            if(queue.size()==1) return -1;
+            //큐에 음식이 하나만 남았다면 더 이상 섞을 음식이 없음. 
+            if(queue.size()==1) return -1; 
             
             queue.add(queue.poll()+(queue.poll()*2));
             answer++;
