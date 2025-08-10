7aa745c461dbe9badf2b1ac7ffe23c0863c9f289
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2025-08-10 10:14:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.80 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 57 MB, 시간: 9.00 ms
+메모리: 73.2 MB, 시간: 8.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 05일 13:19:18
+2025년 08월 10일 10:14:51
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,53 +1,52 @@
 import java.util.*;
-class Solution { 
-    static int[] di ={1,-1,0,0};
-    static int[] dj = {0,0,-1,1};
+
+class Pos{
+    public int i;
+    public int j;
     
-    static class Pos{
-        int i, j, cnt;
-        public Pos(int i, int j, int cnt){
-            this.i = i;
-            this.j = j;
-            this.cnt = cnt;
-        }
+    Pos(int i, int j){
+        this.i = i;
+        this.j = j;
     }
+}
+
+class Solution {
+    static int[][] visited;
+    static int[] di={-1,1,0,0}, dj={0,0,-1,1};
     
     public int solution(int[][] maps) {
-        int answer = 0;
-        int n = maps.length; 
-        int m = maps[0].length;
+        int answer = 0; 
         
-        boolean[][] visited = new boolean[n][m];
+        visited =  new int[maps.length][maps[0].length];
+        answer = bfs(new Pos(0,0), maps);
+        
+        return answer;
+    }
+    
+    public static int bfs(Pos pos, int[][] maps){
         Queue<Pos> queue = new LinkedList<>();
-        queue.add(new Pos(0,0,0)); //세로,가로,이동횟수
-        visited[0][0] = true;
+        
+        queue.offer(pos);
+        visited[pos.i][pos.j] = 1;
         
         while(!queue.isEmpty()){
-            Pos c = queue.poll();
-            if(c.i==n-1 && c.j==m-1) {
-                answer = c.cnt;
-                break;
-            }
+        
+            Pos s = queue.poll();
             
-            for(int d=0;d<di.length;d++){
-                int ni = c.i + di[d];
-                int nj = c.j + dj[d];
+            if(s.i== maps.length-1  && s.j==maps[0].length-1) return visited[s.i][s.j];
+            
+            for(int d=0; d<4; d++){
+                int ni = s.i + di[d];
+                int nj = s.j + dj[d];
                 
-                if(ni>=0 && ni<n && nj>=0 && nj<m){
-                    if(maps[ni][nj] ==1 && !visited[ni][nj]){
-                        visited[ni][nj] = true;
-                        queue.add(new Pos(ni,nj,c.cnt+1));
-                    }
+                if(ni>=0 && ni < maps.length && nj>=0 && nj < maps[0].length &&
+                   maps[ni][nj]==1 && visited[ni][nj]==0){
+                    queue.offer(new Pos(ni,nj));
+                    visited[ni][nj] = visited[s.i][s.j] + 1;
                 }
             }
         }
-        if(answer == 0) {
-            answer = -1;
-        }else{
-            answer+=1;
-        }
-        
-    
-        return answer;
+        return -1;   
     }
+    
 }
