b2e314c8596e94dbabffbc184ef2268a697f90b3
GabeenKim <77683472+GabeenKim@users.noreply.github.com>
2025-08-10 10:48:35 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,27 +1,32 @@
 class Solution {
-    static boolean[] visited;
-    
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        
-        visited = new boolean[n];
-        
-        for(int i=0;i< n;i++){
-            if(!visited[i]){
+        boolean[] visited = new boolean[n]; // 1차원 배열로 방문 여부 체크
+
+        // 모든 컴퓨터를 순회
+        for (int i = 0; i < n; i++) {
+            // 아직 방문하지 않은 컴퓨터라면
+            if (!visited[i]) {
+                // 새로운 네트워크 발견!
                 answer++;
-                dfs(i, computers);
+                // 해당 컴퓨터와 연결된 모든 컴퓨터를 DFS로 탐색
+                dfs(i, computers, visited);
             }
         }
-        
         return answer;
     }
     
-    public void dfs(int x, int[][] arr){
-        visited[x] = true;
-        for(int i=0;i<arr.length;i++){
-            if(!visited[i] && arr[x][i]==1) dfs(i, arr);
+    // DFS 함수
+    public void dfs(int node, int[][] computers, boolean[] visited) {
+        // 현재 노드(컴퓨터)를 방문 처리
+        visited[node] = true;
+
+        // 현재 노드와 연결된 다른 노드들을 탐색
+        for (int i = 0; i < computers.length; i++) {
+            // 연결되어 있고 아직 방문하지 않았다면
+            if (computers[node][i] == 1 && !visited[i]) {
+                dfs(i, computers, visited); // 재귀 호출
+            }
         }
     }
 }
-//방문하지 않은 컴퓨터가 있으면 새로운 네트워크로 간주함. 
-//dfs로 헤당 컴퓨터와 연결된 모든 컴퓨터들을 탐색하면서 방문 처리
